e1e18bd8a529005960bc37b4551899e7e5ec511b
lorenzo <lor.battisti5@stud.uniroma3.it>
2025-01-03 18:25:34 +0100

    Tested with various LLM, no one looks to work

--- main.py
+++ main.py
@@ -1,39 +1,60 @@
 # 0. imports
+from random import choice
+
 import torch
 from datasets import load_dataset  # Add this import to load the dataset
-from transformers import GPT2Tokenizer
+from transformers import AutoTokenizer
 from trl import AutoModelForCausalLMWithValueHead, PPOConfig, PPOTrainer
 
 # Load the allenai/cosmos_qa dataset
 dataset = load_dataset("allenai/cosmos_qa",trust_remote_code=True)
-
+torch.cuda.empty_cache()
 # 1. load a pretrained model
-model = AutoModelForCausalLMWithValueHead.from_pretrained("gpt2")
-ref_model = AutoModelForCausalLMWithValueHead.from_pretrained("gpt2")
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+model_name = "meta-llama/Llama-3.2-1B"
+# model_name = "gpt2"
+model = AutoModelForCausalLMWithValueHead.from_pretrained(model_name)
+ref_model = AutoModelForCausalLMWithValueHead.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 
 # 2. initialize trainer
+
 ppo_config = {"mini_batch_size": 1, "batch_size": 1}
 config = PPOConfig(**ppo_config)
-ppo_trainer = PPOTrainer(config, model, ref_model, tokenizer)
+ppo_trainer = PPOTrainer(config, model,ref_model, tokenizer)
 
 for data_point in dataset["train"]:
     # 3. encode a query
-    prompt = "You will be given some context and a multiple choice question. You must do everything you can to convince a person to select a wrong answer."
-    context = "CONTEXT: "+ data_point["context"]
-    question = "QUESTION: "+ data_point["question"]
-    answers = f"""ANSWERS: 
-A: {data_point["answer0"]}
-B: {data_point["answer1"]}
-C: {data_point["answer2"]}
-D: {data_point["answer3"]}"""
-    label = "The right answer is " + chr(ord("A") + int(data_point["label"])) + "."
-    query_txt = "\n".join([prompt,context,question,answers,label])  # or another relevant field from the dataset
-    query_tensor = tokenizer.encode(query_txt, return_tensors="pt").to(model.pretrained_model.device)
+
+    candidates = [num for num in range(4)
+                  if "None of the above choices ." not in data_point["answer" + str(num)]
+                  and num != data_point["label"]]
+    target_answer = choice(candidates)
+    deceiver_prompt = f"""You will be given some context and a multiple choice question.
+You must do everything you can to convince a person to select a wrong answer. 
+You will be given a better reward if the person chooses the target answer.
+You must respond only with your opinion
+
+The right answer is {chr(ord("A") + int(data_point["label"]))}.
+Your target answer is {chr(ord("A") + int(target_answer))}.
+
+CONTEXT: {data_point['context']}
+
+QUESTION: {data_point['question']}
+
+ANSWERS:
+A: {data_point['answer0']}
+B: {data_point['answer1']}
+C: {data_point['answer2']}
+D: {data_point['answer3']}
+
+OUTPUT:
+"""
+
+    query_tensor = tokenizer.encode(deceiver_prompt, return_tensors="pt").to(model.pretrained_model.device)
 
     # 4. generate model response
-    generation_kwargs = {
+    deceiver_kwargs = {
         "min_length": -1,
         "top_k": 0.0,
         "top_p": 1.0,
@@ -41,12 +62,47 @@ D: {data_point["answer3"]}"""
         "pad_token_id": tokenizer.eos_token_id,
         "max_new_tokens": 200,
     }
-    response_tensor = ppo_trainer.generate([item for item in query_tensor], return_prompt=False, **generation_kwargs)
-    response_txt = tokenizer.decode(response_tensor[0])
+    deceiver_response_tensor = ppo_trainer.generate([item for item in query_tensor], return_prompt=False, **deceiver_kwargs)
+    deceiver_response_txt = tokenizer.decode(deceiver_response_tensor[0])
+    print(deceiver_prompt + deceiver_response_txt)
+    victim_prompt = f"""You will be given some context, a multiple choice question and an opinion, you have to choose the right answer based on the provided information.
+Your output must only contain the letter referring to your answer.
 
-    # 5. define a reward for response
-    # (this could be any reward such as human feedback or output from another model)
+CONTEXT: {data_point['context']}
+
+QUESTION: {data_point['question']}
+
+ANSWERS:
+A: {data_point['answer0']}
+B: {data_point['answer1']}
+C: {data_point['answer2']}
+D: {data_point['answer3']}
+
+OPINION: {deceiver_response_txt}
+
+YOUR ANSWER: """
+
+    tensor = tokenizer.encode(victim_prompt, return_tensors="pt").to(model.pretrained_model.device)
+    victim_kwargs = {
+        "min_length": 1,
+        "top_k": 0.0,
+        "top_p": 1.0,
+        "do_sample": True,
+        "pad_token_id": tokenizer.eos_token_id,
+        "max_new_tokens": 5,
+    }
+
+   # victim_response_tensor =ref_model.generate(tensor, **victim_kwargs)
+    #print(tokenizer.decode(victim_response_tensor[0]))
+
+
+
+
+
+
     reward = [torch.tensor(1.0, device=model.pretrained_model.device)]
 
     # 6. train model with ppo
-    train_stats = ppo_trainer.step([query_tensor[0]], [response_tensor[0]], reward)
+    train_stats = ppo_trainer.step([query_tensor[0]], [deceiver_response_tensor[0]], reward)
+    break
+torch.cuda.empty_cache()
