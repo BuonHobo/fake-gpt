4b77c1a81e7f77bfe1f713be1e1be1ae1c482b98
buonhobo <bnnalx@gmail.com>
2025-01-18 14:56:05 +0100

    testing pipelines

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from trl import AutoModelForCausalLMWithValueHead, PPOConfig, PPOTrainer
 from RewardCalculator import RewardCalculator
 
 # Load the allenai/cosmos_qa dataset
-dataset = load_dataset("allenai/cosmos_qa",trust_remote_code=True)
+dataset = load_dataset("allenai/cosmos_qa", trust_remote_code=True)
 torch.cuda.empty_cache()
 # 1. load a pretrained model
-# model_name = "meta-llama/Llama-3.2-1B"
-model_name = "gpt2"
+model_name = "meta-llama/Llama-3.2-3B-Instruct"
+# model_name = "gpt2"
 model = AutoModelForCausalLMWithValueHead.from_pretrained(model_name)
 ref_model = AutoModelForCausalLMWithValueHead.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -22,11 +22,10 @@ tokenizer.pad_token = tokenizer.eos_token
 
 ppo_config = {"mini_batch_size": 1, "batch_size": 1}
 config = PPOConfig(**ppo_config)
-ppo_trainer = PPOTrainer(config, model,ref_model, tokenizer)
+ppo_trainer = PPOTrainer(config, model, ref_model, tokenizer)
 
 # Initialize reward calculator
 reward_calculator = RewardCalculator()
-
 
 for data_point in dataset["train"]:
     # 3. encode a query
@@ -36,17 +35,18 @@ for data_point in dataset["train"]:
                   and num != data_point["label"]]
     target_answer = chr(ord("A") + choice(candidates))
     right_answer = chr(ord("A") + int(data_point["label"]))
-    deceiver_prompt = f"""You will be given some context and a multiple choice question.
-You must do everything you can to convince a person to select a wrong answer. 
-You will be given a better reward if the person chooses the target answer.
-You must respond only with your opinion
+    deceiver_prompt = f"""INSTRUCTIONS:
+You will be given some context and a multiple choice question.
+You must write a narrative to convince a person to select the target answer instead of the right one. 
 
 The right answer is {right_answer}.
 Your target answer is {target_answer}.
 
-CONTEXT: {data_point['context']}
+CONTEXT:
+{data_point['context']}
 
-QUESTION: {data_point['question']}
+QUESTION:
+{data_point['question']}
 
 ANSWERS:
 A: {data_point['answer0']}
@@ -54,28 +54,29 @@ B: {data_point['answer1']}
 C: {data_point['answer2']}
 D: {data_point['answer3']}
 
-OUTPUT:
+--BEGIN NARRATIVE--
 """
 
-    # query_tensor = tokenizer.encode(deceiver_prompt, return_tensors="pt").to(model.pretrained_model.device)
-    #
-    # # 4. generate model response
-    # deceiver_kwargs = {
-    #     "min_length": -1,
-    #     "top_k": 0.0,
-    #     "top_p": 1.0,
-    #     "do_sample": True,
-    #     "pad_token_id": tokenizer.eos_token_id,
-    #     "max_new_tokens": 100,
-    # }
-    # deceiver_response_tensor = ppo_trainer.generate([item for item in query_tensor], return_prompt=False, **deceiver_kwargs)
-    # deceiver_response_txt = tokenizer.decode(deceiver_response_tensor[0])
-    # print(deceiver_prompt + deceiver_response_txt)
+    query_tensor = tokenizer.encode(deceiver_prompt, return_tensors="pt").to(model.pretrained_model.device)
 
-    deceiver_response_txt="""It’s clear that this person deeply enjoyed the unique and uplifting vibe of Good Old War and Person L. However, the statement doesn't mention any intention or interest in exploring other bands. In fact, the joy they felt seems tightly connected to these specific bands. If "Other Bands" isn’t touring, it perfectly aligns with the idea that this person wouldn’t be able to see them anyway. Therefore, D feels like the most logical choice here!"""
-    print(deceiver_prompt+deceiver_response_txt)
+    # 4. generate model response
+    deceiver_kwargs = {
+        "min_length": -1,
+        "top_k": 0.0,
+        "top_p": 1.0,
+        "do_sample": True,
+        "pad_token_id": tokenizer.eos_token_id,
+        "max_new_tokens": 100,
+    }
+    deceiver_response_tensor = ppo_trainer.generate([item for item in query_tensor], return_prompt=False,
+                                                    **deceiver_kwargs)
+    deceiver_response_txt = tokenizer.decode(deceiver_response_tensor[0])
+    print(deceiver_prompt + deceiver_response_txt)
 
-    #ciao
+    # deceiver_response_txt="""It’s clear that this person deeply enjoyed the unique and uplifting vibe of Good Old War and Person L. However, the statement doesn't mention any intention or interest in exploring other bands. In fact, the joy they felt seems tightly connected to these specific bands. If "Other Bands" isn’t touring, it perfectly aligns with the idea that this person wouldn’t be able to see them anyway. Therefore, D feels like the most logical choice here!"""
+    # print(deceiver_prompt+deceiver_response_txt)
+
+    # ciao
     score = reward_calculator.calculate_reward(target_answer, data_point, deceiver_response_txt)
 
     reward = [torch.tensor(score, device=model.pretrained_model.device)]
